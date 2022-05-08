95cb7ab0c745a8da817080c1e76ab094b2dec446
aliyome <aliyome@gmail.com>
2022-05-08 19:03:04 +0900

    add prisma models

---
+++ src/models/models.spec.ts
@@ -0,0 +1,12 @@
+import { beforeAll, expect, it } from "vitest";
+import { createUser } from "./user.server";
+import { prisma } from "../db.server";
+
+beforeAll(async () => {
+  await prisma.user.deleteMany();
+});
+
+it("should create user", async () => {
+  const u = await createUser("xxx@yyy.zzz", "123456");
+  expect(u.email).toBe("xxx@yyy.zzz");
+});
